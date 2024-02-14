9d417ab94fe2bd45b59d326fd1bcc3931fd0c87c
Jan P Buchmann <jan.buchmann@hhu.de>
2024-02-14 09:18:36 +0100

    Changing viewer to report GFF as TSV

--- src/commands/viewer.cpp
+++ src/commands/viewer.cpp
@@ -1,104 +1,106 @@
 /*
  * \author Jan Piotr Buchmann <jpb@members.fsf.org>
  * \copyright 2022
-*/
+ */
 
 #include "commands/viewer.h"
-
-#include <iostream>
-#include <climits>
-#include <algorithm>
 
 #include "commands/command.h"
 #include "gfffile.h"
 
+#include <algorithm>
+#include <climits>
+#include <iostream>
+
 namespace gff
 {
-  Viewer::Viewer()
-  {  }
+Viewer::Viewer() {}
 
-  Viewer::~Viewer()
-  {  }
+Viewer::~Viewer() {}
 
-  void Viewer::usage()
+void Viewer::usage()
+{
+  std::cout << "Create compact view of GFF file\n"
+            << "\t--input, -i <path>         Path to GFF file\n"
+            << "\t--source, -s <STR>         Optional: name of organism / "
+               "annotation source\n"
+            << "\t--help, -h                 Show help\n";
+}
+
+void Viewer::run()
+{
+  gff::GffFile gff(gff_file);
+  gff.parse(*this);
+}
+
+const std::string& Viewer::description() { return descr; }
+
+const std::string& Viewer::command() { return name; }
+
+int Viewer::setup(int argc, char** argv)
+{
+  std::string sopt;
+  while(true)
   {
-    std::cout <<  "Create compact view of GFF file\n"
-              <<  "\t--input, -i <path>         Path to GFF file\n"
-              <<  "\t--help, -h                 Show help\n";
-    exit(1);
-  }
+    const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+    if(opt == -1) break;
 
-  void Viewer::run()
-  {
-    gff::GffFile gff(gff_file);
-    gff.parse(*this);
-  }
-
-  const std::string& Viewer::description()
-  {
-    return descr;
-  }
-
-  const std::string& Viewer::command()
-  {
-    return name;
-  }
-
-  int Viewer::setup(int argc, char **argv)
-  {
-    std::string sopt;
-    while(true)
+    switch(opt)
     {
-      const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
-      if (opt == -1)
+      case 'i':
+        gff_file = optarg;
         break;
 
-      switch (opt)
-      {
-        case 'i':
-          gff_file = optarg;
-          break;
+      case 's':
+        source = std::string(optarg);
+        break;
 
-        case 'h': // -h or --help
-          usage();
-          return EXIT_SUCCESS;
+      case 'h':
+        usage();
+        return EXIT_SUCCESS;
 
-        case '?': // Unrecognized option
-          return EXIT_FAILURE;
+      case '?': // Unrecognized option
+        return EXIT_FAILURE;
 
-        default:
-          usage();
-          break;
-      }
+      default:
+        usage();
+        break;
     }
-    test_input_file();
-    return EXIT_SUCCESS;
   }
+  test_input_file();
+  return EXIT_SUCCESS;
+}
 
-  int Viewer::process_locus(gff::TypeFeature* locus)
-  {
-    compact_view(locus);
-    return EXIT_SUCCESS; // temporary
-  }
+int Viewer::process_locus(gff::TypeFeature* locus)
+{
+  compact_view(locus, locus->id);
+  return EXIT_SUCCESS; // temporary
+}
 
-  void Viewer::compact_view(const gff::Feature* feat, int level)
+void Viewer::compact_view(const gff::Feature* feat,
+                          const std::string_view& locus_id, int level)
+{
+  // std::cout << std::string(level, '\t') << feat->type << ":" << feat->id
+  //           << "\n";
+  std::cout << locus_id << "\t" << feat->type << "\t" << feat->id << "\t"
+            << feat->start() << "\t" << feat->end();
+  if(!source.empty()) { std::cout << "\t" << source; }
+  std::cout << "\n";
+  if(!feat->get_children().empty())
   {
-    std::cout << std::string(level, '\t') << feat->type << ":" << feat->id <<"\n";
-    if(!feat->get_children().empty())
+    ++level;
+    for(const auto& i : feat->get_children())
     {
-      ++level;
-      for(const auto& i : feat->get_children())
+      // std::sort(i.second.begin(), i.second.end(),[](const gff::Feature* lhs,
+      // const gff::Feature* rhs)
+      // {return lhs->length() > rhs->length();});
+      for(const auto& j : i.second)
       {
-        // std::sort(i.second.begin(), i.second.end(),[](const gff::Feature* lhs,
-        // const gff::Feature* rhs)
-        // {return lhs->length() > rhs->length();});
-        for(const auto& j : i.second)
-        {
-          compact_view(j.second, level);
-        }
+        compact_view(j.second, locus_id, level);
       }
-      --level;
     }
+    --level;
   }
+}
 
 } // namespace gff
--- src/commands/viewer.h
+++ src/commands/viewer.h
@@ -5,37 +5,41 @@
 
 #pragma once
 
-#include <iostream>
-#include <climits>
-#include <algorithm>
-
 #include "commands/command.h"
 #include "gfffile.h"
 
+#include <algorithm>
+#include <climits>
+#include <iostream>
+
 namespace gff
 {
-  class Viewer : public Command, GffFile::Processor
-  {
-    public:
-      Viewer();
-      ~Viewer();
-      int setup(int argc, char **argv);
-      void usage();
-      void run();
-      const std::string& description();
-      const std::string& command();
-      int process_locus(gff::TypeFeature* locus);
+class Viewer
+  : public Command
+  , GffFile::Processor
+{
+  public:
+    Viewer();
+    ~Viewer();
+    int setup(int argc, char** argv);
+    void usage();
+    void run();
+    const std::string& description();
+    const std::string& command();
+    int process_locus(gff::TypeFeature* locus);
 
-    private:
-      const std::string descr = "View compact version of GFF";
-      const std::string name = "view";
-      const char* const short_opts = "i:h";
-      const option long_opts[3] =
-      {
-        {"input", required_argument, nullptr, 'i'},
-        {"help", no_argument, nullptr, 'h'},
-        {nullptr, no_argument, nullptr, 0}
-      };
-      void compact_view(const gff::Feature* feat, int level = 0);
-  };
+  private:
+    const std::string descr = "View compact version of GFF";
+    const std::string name = "view";
+    std::string source;
+    const char* const short_opts = "i:hs:";
+    const option long_opts[4] = {
+      { "input", required_argument, nullptr, 'i'},
+      {  "help",       no_argument, nullptr, 'h'},
+      {"source", optional_argument, nullptr, 's'},
+      { nullptr,       no_argument, nullptr,   0}
+    };
+    void compact_view(const gff::Feature* feat,
+                      const std::string_view& locus_id, int level = 0);
+};
 } // namespace gff
